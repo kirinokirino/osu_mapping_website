90e1cfd0fbdfdab64e2bf754ac51173025474fa2
2501 <kirinokirino2501@yahoo.com>
2018-01-23 21:53:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
     $("sidebar a").on("click", function(event) {
         $("#sidebar_selected_item").removeAttr("id");
         $(this).attr("id", "sidebar_selected_item");
+        document.title = $("#sidebar_selected_item").text();
     });
  }); 
